4f98038fa506160a4f45615334c1f3095f24cd02
aditipatel <pate1452@purdue.edu>
2024-07-05 17:59:39 -0400

    added extra credit functionalities: random color, erase, and clear
    - also changed color theme and fonts

--- js-file.js
+++ js-file.js
@@ -9,6 +9,15 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
+    function getRandomColor() {
+        var letters = '0123456789ABCDEF';
+        var color = '#';
+        for (var i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }
+
     function createGrid(gridSize) {
         for (let i = 0; i < gridSize; i++) {
             row = document.createElement("div");
@@ -28,10 +37,28 @@ document.addEventListener('DOMContentLoaded', () => {
 
         gridItems.forEach((item) => {
             item.addEventListener("mouseover", () => {
-                item.style.backgroundColor = 'pink'; 
+                item.style.backgroundColor = 'pink';
             });
 
         });
+
+        const randomColorButton = document.getElementById("random-color-button")
+        randomColorButton.addEventListener("click", () => {
+            gridItems.forEach((item) => {
+                item.addEventListener("mouseover", () => {
+                    item.style.backgroundColor = getRandomColor();
+                });
+            });
+        })
+
+        const eraseButton = document.getElementById("erase-button")
+        eraseButton.addEventListener("click", () => {
+            gridItems.forEach((item) => {
+                item.addEventListener("mouseover", () => {
+                    item.style.backgroundColor = "rgb(247, 235, 217)";
+                });
+            });
+        })
     }
 
     createGrid(gridSize);
@@ -39,18 +66,27 @@ document.addEventListener('DOMContentLoaded', () => {
     const body = document.querySelector("body");
     const curSize = document.createElement("p");
     curSize.className = "curSize";
-    curSize.textContent = "Current Grid Size: " + gridSize + " x " + gridSize; 
+    curSize.textContent = "Current Grid Size: " + gridSize + " x " + gridSize;
     body.appendChild(curSize);
 
     const changeSize = document.querySelector("button");
     changeSize.addEventListener("click", () => {
-        let size = prompt("Enter a grid size between 1-100");
-        if (parseInt(size) >= 1 && parseInt(size) <= 100) {
+        gridSize = prompt("Enter a grid size between 1-100");
+        if (parseInt(gridSize) >= 1 && parseInt(gridSize) <= 100) {
             clearGrid();
-            createGrid(size);
-            curSize.textContent = "Current Grid Size: " + size + " x " + size; 
+            createGrid(gridSize);
+            curSize.textContent = "Current Grid Size: " + gridSize + " x " + gridSize;
         } else {
             alert("Please enter a vaue between 1-100")
         }
     })
+
+    const clearButton = document.getElementById("clear-button")
+    clearButton.addEventListener("click", () => {
+        clearGrid();
+        createGrid(gridSize);
+
+    })
+
+
 });
